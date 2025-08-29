fdb55bd1e2c3fd69923a572d6fbb1654c24e8088
emontzka <emontzka@jnlcom.com>
2025-08-29 11:11:20 -0500

    add enable/disable date acf

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -10,8 +10,12 @@
 ?>
 <?php if ( visualcomposerstarter_is_the_title_displayed() && get_the_title() ) : ?>
 <h1 class="entry-title"><?php the_title(); ?></h1>
-<?php endif; ?>
-<div class="date"><?php echo get_the_date( 'M j, Y'); ?> | <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>"><span class="author vcard"><?php esc_html_e(get_the_author()); ?></span></a></div>
+<?php endif; 
+$disable_date = get_field('disable_date'); ?>
+<div class="date">
+	<?php if ($disable_date == 'enable') {echo get_the_date( 'M j, Y').' | ';} ?> 
+	<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>"><span class="author vcard"><?php esc_html_e(get_the_author()); ?></span></a>
+</div>
 
 <div class="entry-content">
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
