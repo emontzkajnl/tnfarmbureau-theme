64e85c97c20e1b1aba8e54f20c06eb8cf9807161
emontzka <emontzka@jnlcom.com>
2025-12-04 09:36:34 -0600

    add podcast function to archive template

--- template-parts/content.php
+++ template-parts/content.php
@@ -12,13 +12,23 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'entry-preview' ); ?>>
 	<div class="object-fit-image">
 	<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?> ">
-	<?php //visualcomposerstarter_post_thumbnail(); 
+	<?php 
 	the_post_thumbnail();?>
 	</a></div>
 
 	<?php //visualcomposerstarter_entry_meta(); ?>
 
 	<div class="entry-content">
+		<?php if (function_exists('the_powerpress_content')) {
+			if (is_category('podcast')) {
+				the_powerpress_content('podcast');
+			} elseif (is_category( 'farm-bureau-on-the-hill' )) {
+				the_powerpress_content('farm-bureau-on-the-hill-podcast');
+			} elseif (is_category( 'root-of-the-matter' )) {
+				the_powerpress_content('root-of-the-matter-podcast');
+			}
+		}
+		?>
 		<?php echo '<p class="latest-news__date">'.get_the_date('M j, Y').'</p>'; ?>
 		<?php the_title( sprintf( '<h2 class="archive-entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
